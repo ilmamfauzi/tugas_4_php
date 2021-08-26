134d371ec63d30108a6ce69d3aeb16d7953f795b
Ilmam Fauzi <ilmamfauzi@gmail.com>
2021-08-26 23:09:36 +0700

    add file tugas4_2

---
+++ tugas4_2.php
@@ -0,0 +1,10 @@
+<?php
+$jml_saluran = 523;
+$pemakaian = 8891;
+
+$ratarata_pakai = $pemakaian / $jml_saluran;
+
+echo "Jumlah Saluran = $jml_saluran<br>";
+echo "Jumlah Terpakai = $pemakaian<br>";
+echo "Rata-rata pemakaian = ?<br>";
+echo "Jawaban = $ratarata_pakai liter/keluarga";
